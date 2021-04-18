eea4a4662f967fa549729ad284d2e6d16a7965bb
FahiAbdelMalek <abdelmalek.fathi.2001@gmail.com>
2021-04-18 01:28:17 +0100

    nothing important, just some improvement in submissions confirmation and delete unused code

--- conferences/models.py
+++ conferences/models.py
@@ -8,7 +8,7 @@ User = get_user_model()
 class Author(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    email = models.EmailField(verbose_name="email", max_length=254, unique=True)
+    email = models.EmailField(verbose_name="email", max_length=254, unique=False)
 
     def __str__(self):
         return self.last_name + " " + self.first_name
