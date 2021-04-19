7a3b97247b3d2329d5c0ded8271690efbb1a3cad
FahiAbdelMalek <abdelmalek.fathi.2001@gmail.com>
2021-04-19 14:23:24 +0100

    change venv

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!d:\projects\pythonprojects\memoirprojet\venv\scripts\python.exe
+#!D:\Projects\pythonProjects\MemoirProjet\venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
