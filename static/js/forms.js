b48ed31692345dc4b6060fc12fb51f9a64773334
FathiAbdelMalek <abdelmalek.fathi.2001@gmail.com>
2021-05-19 09:07:29 +0100

    half of the work on the forms

--- static/js/forms.js
+++ static/js/forms.js
@@ -1,7 +1,20 @@
 function openForm() {
-  document.getElementById("myForm").style.display = "block";
+  document.getElementById("registerForm").style.display = "block";
 }
 
 function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+  document.getElementById("registerForm").style.display = "none";
+}
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
