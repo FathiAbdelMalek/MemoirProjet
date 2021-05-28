60a20b04348f5ceed8ac713537e64d224856d411
FathiAbdelMalek <abdelmalek.fathi.2001@gmail.com>
2021-05-28 18:10:32 +0100

    config for heroku

--- MemoirProjet/settings.py
+++ MemoirProjet/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'eii91w9^)b273!t-2aw%*sc(r2$!^pexy)wez4yy&m$2htjs39'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn MemoirProjet.wsgi --log-file
