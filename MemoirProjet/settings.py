f0a45d9f5e2b35837a9f3a480d87a88fd3581a2b
FathiAbdelMalek <abdelmalek.fathi.2001@gmail.com>
2021-05-29 22:05:10 +0100

    some fixes

--- MemoirProjet/settings.py
+++ MemoirProjet/settings.py
@@ -148,9 +148,9 @@ LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'abdelmalek.fathi.2001@gmail.com'
-EMAIL_HOST_PASSWORD = 'Fathi_CJPP@2001'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'abdelmalek.fathi.2001@gmail.com'
+# EMAIL_HOST_PASSWORD = 'Fathi_CJPP@2001'
