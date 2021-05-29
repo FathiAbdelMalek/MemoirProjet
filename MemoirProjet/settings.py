745ffd90eab2d0837bf55ffe5ef56bb537b01cd4
FathiAbdelMalek <abdelmalek.fathi.2001@gmail.com>
2021-05-29 21:42:03 +0100

    some fixes

--- MemoirProjet/settings.py
+++ MemoirProjet/settings.py
@@ -94,9 +94,9 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-db = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db)
+# import dj_database_url
+# db = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -134,10 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
