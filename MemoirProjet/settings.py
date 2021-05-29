9ca6924bf6712e68084651df35c7b9f83e178e3d
FathiAbdelMalek <abdelmalek.fathi.2001@gmail.com>
2021-05-29 19:48:04 +0100

    some fixes

--- MemoirProjet/settings.py
+++ MemoirProjet/settings.py
@@ -94,9 +94,9 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-db = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db)
+# import dj_database_url
+# db = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
